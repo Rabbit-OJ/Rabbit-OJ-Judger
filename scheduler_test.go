5134ae9fc80822047f92cad2ea1202b622a1617f
ttzztztz <ttzztztz@outlook.com>
2020-09-22 14:18:57 +0800

    test: unit test for rust

--- scheduler_test.go
+++ scheduler_test.go
@@ -75,7 +75,7 @@ func initJudger() {
 		Languages: []JudgerModels.LanguageType{
 			{
 				ID:      "cpp17",
-				Name:    "C++17",
+				Name:    "C++/17",
 				Enabled: true,
 				Args: JudgerModels.CompileInfo{
 					BuildArgs: []string{
@@ -94,6 +94,35 @@ func initJudger() {
 					NoBuild:     false,
 					BuildTarget: "/home/code.o",
 					BuildImage:  "gcc:10.2.0",
+					Constraints: JudgerModels.Constraints{
+						CPU:          1000000000,
+						Memory:       1073741824,
+						BuildTimeout: 120,
+						RunTimeout:   120,
+					},
+					RunArgs:     []string{"/home/code.o"},
+					RunArgsJSON: "[\"/home/code.o\"]",
+					RunImage:    "alpine_tester:latest",
+				},
+			},
+			{
+				ID:      "rust",
+				Name:    "Rust/1.46",
+				Enabled: true,
+				Args: JudgerModels.CompileInfo{
+					BuildArgs: []string{
+						"rustc",
+						"-O",
+						"/home/code.rs",
+						"-o",
+						"/home/code.o",
+						"--target",
+						"x86_64-unknown-linux-musl",
+					},
+					Source:      "/home/code.rs",
+					NoBuild:     false,
+					BuildTarget: "/home/code.o",
+					BuildImage:  "rust:alpine",
 					Constraints: JudgerModels.Constraints{
 						CPU:          1000000000,
 						Memory:       1073741824,
@@ -493,6 +522,41 @@ func TestPython3ShouldEmitAC(t *testing.T) {
 	code := []byte("s = input().split()\n" +
 		"print(int(s[0]) + int(s[1]))\n")
 	status, judgeResult, _ := testJudgeHelper(code, "python3")
+
+	if status != "OK" {
+		fmt.Println("[Should Emit AC] Status NOT OK")
+		t.Fail()
+		return
+	}
+	for _, result := range judgeResult {
+		if result.Status != "AC" {
+			fmt.Println("[Should Emit AC] Some Case Status NOT AC", result)
+			t.Fail()
+			return
+		}
+	}
+}
+
+func TestRustShouldEmitAC(t *testing.T) {
+	defer func() {
+		if err := recover(); err != nil {
+			fmt.Printf("%+v \n", err)
+			t.Fail()
+		}
+	}()
+
+	code := []byte("use std::io;\n\n" +
+		"fn main(){\n" +
+		"    let mut input=String::new();\n" +
+		"    io::stdin().read_line(&mut input).unwrap();\n" +
+		"    let mut s=input.trim().split(' ');\n\n" +
+		"    let a:i32=s.next().unwrap()\n" +
+		"               .parse().unwrap();\n" +
+		"    let b:i32=s.next().unwrap()\n" +
+		"               .parse().unwrap();\n" +
+		"    println!(\"{}\",a+b);" +
+		"\n}")
+	status, judgeResult, _ := testJudgeHelper(code, "rust")
 
 	if status != "OK" {
 		fmt.Println("[Should Emit AC] Status NOT OK")
